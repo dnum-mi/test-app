7842e1846026be4e1f9d077213acd6763b9ecd3f
this-is-tobi <this-is-tobi@proton.me>
2026-02-06 13:28:43 +0100

    fix: fix-018

--- apps/client/src/api.ts
+++ apps/client/src/api.ts
@@ -3,8 +3,13 @@ const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:8081/api'
 export function callApi(btn: HTMLButtonElement, msg: HTMLParagraphElement) {
   const getMsg = async () => {
     const res = await fetch(apiUrl)
-    const { docUrl, githubUrl } = JSON.parse(await res.text())
-    msg.innerHTML = `Welcome aboard !<br/><br/>Visit the <a href="${docUrl}" target="_blank">documentation</a> and the <a href="${githubUrl}" target="_blank">Github organization</a>.`
+    const { githubUrls } = JSON.parse(await res.text())
+    
+    const urlLinks = githubUrls
+      .map((url: string) => `<a href="${url}" target="_blank">${url}</a>`)
+      .join('<br/>')
+    
+    msg.innerHTML = `Welcome aboard !<br/><br/>${urlLinks}`
   }
   btn.addEventListener('click', () => getMsg())
 }
